2022e20317c13c20086ade9c5e18f421db6e32bb
Leo Rosales <leorosal@usc.edu>
2025-09-04 12:48:20 +0900

    feat: update website content [2025-09-04T03:48:19.928Z]

--- src/data/staticContent.js
+++ src/data/staticContent.js
@@ -1,6 +1,6 @@
 
 // This file is auto-generated. Do not edit directly.
-// Last updated: 2025-09-04T03:46:04.933Z
+// Last updated: 2025-09-04T03:48:19.755Z
 
 export const siteContent = {
   "members": [],
